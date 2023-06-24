8b2c4fe894f7de847db9627c4cf8eee875bffdc2
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-06-24 16:36:15 +0700

    Migrate to Svelte

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,15 +1,5 @@
-import { isSusDomain } from "./checkUrl";
+import { storage } from "../storage";
 
 chrome.runtime.onInstalled.addListener(() => {
-  // default state goes here
-  // this runs ONE TIME ONLY (unless the user reinstalls your extension)
-  console.log('Get Motivated installed!');
-});
-
-chrome.commands.onCommand.addListener((command) => {
-  // do something when the user presses your keyboard shortcut
-});
-
-chrome.omnibox.onInputEntered.addListener((text) => {
-  console.log(isSusDomain(text));
+    storage.get().then(console.log);
 });
