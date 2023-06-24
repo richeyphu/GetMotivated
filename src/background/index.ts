2c538f54b3c5150ca31040a8ef89541e720e89eb
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-06-24 17:52:49 +0700

    Add check sus domain

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,5 +1,23 @@
-import { storage } from "../storage";
+import { storage } from '../storage';
+import { isSusDomain } from '../utils';
 
 chrome.runtime.onInstalled.addListener(() => {
-    storage.get().then(console.log);
+  console.log('Get Motivated installed!');
+  storage.get().then(console.log);
 });
+
+chrome.omnibox.onInputEntered.addListener((text) => {
+  console.log(text);
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.status == 'complete' && tab.active) {
+    console.log(tab.url);
+    
+    if (await isSusDomain(tab.url)) {
+      console.log('sus domain');
+    } else {
+      console.log('not sus domain');
+    }
+  }
+})
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { isSusDomain } from './checkUrl';
