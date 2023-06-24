2e3819ae33eb3132f07504e9bbac46187b1e7ee4
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-06-25 02:36:43 +0700

    handle dns fetch error

--- src/utils/checkUrl.ts
+++ src/utils/checkUrl.ts
@@ -5,19 +5,27 @@ export const isSusDomain = async (url: string) => {
     return true;
   }
 
-  const dnsUrl = `https://family.cloudflare-dns.com/dns-query?name=${name}`;
-  const response = await fetch(dnsUrl, {
-    headers: {
-      Accept: 'application/dns-json',
-    },
-  });
-  const json = await response.json();
+  try {
+    const dnsUrl = `https://family.cloudflare-dns.com/dns-query?name=${name}`;
+    const response = await fetch(dnsUrl, {
+      headers: {
+        Accept: 'application/dns-json',
+      },
+    });
+    const json = await response.json();
 
-  if (!json.Answer) {
-    return false;
+    if (!json.Answer) {
+      return false;
+    }
+
+    return json.Answer[0].data === '0.0.0.0';
+  } catch (e) {
+    console.error(e);
+    return true;
   }
-
-  return json.Answer[0].data === '0.0.0.0';
 };
 
-export const susDomains = ['hifumin.app'];
+export const susDomains = [
+  // Add custom sus domains here
+  'hifumin.app',
+];
